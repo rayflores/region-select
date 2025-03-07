d3223d364aa5cb6040a67ab81620cc0c14019146
rayflores <rayflores88@gmail.com>
2025-03-07 11:32:56 -0500

    bg color and typo css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'c9c143e6a968aa7516a4');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '593f321e39fc9e06ba56');
