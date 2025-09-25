9a39a97d148f4a315b598155ebd7a1812acc29d6
rayflores <rayflores88@gmail.com>
2025-09-25 11:21:26 -0400

    ver bump

--- class-regionselect.php
+++ class-regionselect.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Region Select
  * Description: A simple plugin to add a region select field to the website.
- * Version: 1.6.0
+ * Version: 1.6.1
  * Author: Ray Flores
  * Author URI: https://rayflores.com
  * License: GPL2
