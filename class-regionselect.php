82dd5a8829515004e706d0e761ddcfd4aa8c2865
rayflores <rayflores88@gmail.com>
2025-09-26 10:29:28 -0400

    adding cookie check to region-select check

--- class-regionselect.php
+++ class-regionselect.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Region Select
  * Description: A simple plugin to add a region select field to the website.
- * Version: 1.7.2
+ * Version: 1.7.3
  * Author: Ray Flores
  * Author URI: https://rayflores.com
  * License: GPL2
@@ -62,7 +62,7 @@ class RegionSelect {
 		}
 
 		// If region-select param is present, don't redirect - let the React component handle it
-		if ( isset( $_GET['region-select'] ) ) {
+		if ( isset( $_GET['region-select'] ) && ! isset( $_COOKIE['selectedRegion'] ) ) {
 			return;
 		}
 
